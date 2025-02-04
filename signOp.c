984e861850af905c9e124ebedb5c98fe46c78755
kmugisha244gmail <kmugisha244@gmail.com>
2025-02-04 15:29:24 +0200

    Add files via upload

---
+++ signOp.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main() {
+int a = 10, b = 3;
+// Arithmetic operations
+printf("Addition: %i + %i = %i\n", a, b, a + b);
+printf("Subtraction: %d - %d = %d\n", a, b, a - b);
+printf("Multiplication: %i * %i = %i\n", a, b, a * b);
+printf("Division: %i / %i = %i\n", a, b, a / b);
+return 0;
+}
+
+
